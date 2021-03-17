1c9181a828762a6ac3d37d286df7d0053864b74d
YeonSeong-Lee <62806979+YeonSeong-Lee@users.noreply.github.com>
2021-03-17 22:44:31 +0900

    app source / ver3. 캐쉬 허용으로 바꿔봄.

--- app/src/main/java/com/hufslifeacademy/MainActivity.java
+++ app/src/main/java/com/hufslifeacademy/MainActivity.java
@@ -46,11 +46,12 @@ public class MainActivity extends AppCompatActivity {
         mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
         mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
         mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
-        mWebSettings.setSupportZoom(true); // 화면 줌 허용 여부
-        mWebSettings.setBuiltInZoomControls(true); // 화면 확대 축소 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
         mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
-        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); // 브라우저 캐시 허용 여부
         mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
 
 
         mWebView.loadUrl("https://hufslifeacademy.com"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
