68e29c53fa5ce9d62f719c52e575a6f1d3b4e129
YeonSeong-Lee <62806979+YeonSeong-Lee@users.noreply.github.com>
2021-03-17 23:39:54 +0900

    app source / ver3. 캐쉬 X 화면 확대 불허 파일 접근 허용

--- app/src/main/java/com/hufslifeacademy/MainActivity.java
+++ app/src/main/java/com/hufslifeacademy/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         mWebView = (WebView) findViewById(R.id.webView);
 
         mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+
         mWebSettings = mWebView.getSettings(); //세부 세팅 등록
         mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
         mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
         mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
         mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
-        mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); // 브라우저 캐시 허용 여부
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
         mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
         mWebSettings.setAppCacheEnabled(true);
         mWebSettings.setAllowContentAccess(true);
