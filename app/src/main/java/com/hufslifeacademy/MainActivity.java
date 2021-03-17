55a4b5d4a53cc4bdb3931085e048da0ad73762b7
YeonSeong-Lee <62806979+YeonSeong-Lee@users.noreply.github.com>
2021-03-17 22:51:16 +0900

    app source / ver3. 캐쉬 허용 새창 허용 화면 확대 불허

--- app/src/main/java/com/hufslifeacademy/MainActivity.java
+++ app/src/main/java/com/hufslifeacademy/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         mWebSettings = mWebView.getSettings(); //세부 세팅 등록
         mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
         mWebSettings.setSupportMultipleWindows(true); // 새창 띄우기 허용 여부
-        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
         mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
         mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
         mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
